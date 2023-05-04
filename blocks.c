c4d62ec9571d5690b6ccf11a692ceadfab81a517
a102257 <a102257@alunos.uminho.pt>
2023-05-04 11:14:09 +0100

    imprime iswall

--- blocks.c
+++ blocks.c
@@ -16,23 +16,20 @@ void insertBlock(int a) {
    // enable color output
     start_color();
 
-    // define a color pair with white foreground and black background
-    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    // define a cor, nº é o nº do par para chamar quando queremos usar, a primeira cor é a cor do caracter, a segunda cor é a cor de fundo
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);  // cores para parede
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);  //cores para chao
+    init_pair(3, COLOR_YELLOW, COLOR_RED);   // cores para cavernas
 
-    // turn on the color pair
-   if (a == 0){
-      attron(COLOR_PAIR(1));
-      printw ("%c", FLOOR);
-      attroff(COLOR_PAIR(1));
-   } 
-      if (a == 1){
-      attron(COLOR_PAIR(1));
-      printw ("%c", CAVE);
-      attroff(COLOR_PAIR(1));
-   } 
-      if (a == 2){
+    // turn on the color pair 
+
+     if (a == 1){
       attron(COLOR_PAIR(1));
       printw ("%c", WALL);
       attroff(COLOR_PAIR(1));
+   } else {
+      attron(COLOR_PAIR(2));
+      printw ("%c", FLOOR);
+      attroff(COLOR_PAIR(2));
    }
 }
