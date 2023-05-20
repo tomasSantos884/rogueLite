d7e99ddf95c76fb338f1ad036889d321dad65009
a104172 <a104172@alunos.uminho.pt>
2023-05-20 16:21:11 +0100

    jogador comentado

--- jogador.c
+++ jogador.c
@@ -1,16 +1,16 @@
-//atualizar funcao consoante a struct player
-
-
 //tudo relacionado com o jogador, movimentacao, interacoes etc
-//#include "blocks.c"
+
 #include <curses.h>
 #include <ncurses.h>
 #include "state.h"
 
-void spawnPlayer(BLOCK* map, STATE* st) {
+// A função 'spawnPlayer' gera a posição inicial do jogador no mapa, obtendo coordenadas aleatóriamente selecionadas
+// dentro dos limites do mapa, na condição de que o jogador não incia o jogo dentro de uma parede.
+
+void spawnPlayer(BLOCK* map, STATE* st) { 
     srand(time(NULL));
 
-    int i = rand() % st->nRows;
+    int i = rand() % st->nRows;         // geração de números aleatórios para i e j separadamente
     int j = rand() % st->nCols;
 
     while (map[i * st->nCols + j].isWall == 1) {
@@ -18,17 +18,23 @@ void spawnPlayer(BLOCK* map, STATE* st) {
         j = rand() % st->nCols;
     }
 
-    st->playerX = i;
+    st->playerX = i;   //dá valor às coordenadas da STATE
     st->playerY = j;
 }
+
+// 'updatePosPlayer' é uma função auxiliar de 'update', serve para impôr a condição de não entrar numa parede e para mudar 
+// os valores correspondentes às coordenadas do jogador na struck state definida.
+
  void updatePosPlayer(STATE *st,BLOCK *map, int dx, int dy) {
 
-  if (map[(st->playerX + dx) * st->nCols + (st->playerY + dy)].isWall != 1){
+  if (map[(st->playerX + dx) * st->nCols + (st->playerY + dy)].isWall != 1){ //condição
     st->playerX += dx;
     st->playerY += dy;
   }
 }
 
+// A função 'update' disponibiliza o movimento do jogador em todas as direções através da função 'getch()' e modifica 
+// as suas coordenadas de acordo com a tecla pressionada. Utiliza as setas no teclado e o keypad.
 
 void update(STATE *st,BLOCK *map) {
 	int key = getch();
@@ -36,7 +42,7 @@ void update(STATE *st,BLOCK *map) {
 	mvaddch(st->playerX, st->playerY, ' ');
 	switch(key) {
 		case KEY_A1:
-		case '7': updatePosPlayer(st,(BLOCK*)map, -1, -1); break;
+		case '7': updatePosPlayer(st,(BLOCK*)map, -1, -1); break;  
 		case KEY_UP:
 		case '8': updatePosPlayer(st,(BLOCK*)map, -1, +0); break;
 		case KEY_A3:
@@ -54,7 +60,6 @@ void update(STATE *st,BLOCK *map) {
 		case KEY_C3:
 		case '3': updatePosPlayer(st,(BLOCK*)map, +1, +1); break;
 		case 'q': endwin(); exit(0); break;
-		case 'p': st->playing = false; break;
 	}
 }
 
